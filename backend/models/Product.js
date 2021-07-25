499d02dad91e633ebe8f97ab785d78e020b5094d
David Kostuchenko <davidkost1999@gmail.com>
2021-07-25 17:56:40 +0300

    [Products] products Schema

---
+++ backend/models/Product.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("product", productSchema);
